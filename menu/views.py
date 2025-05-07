b0fbf193fab2ecfc6b456496e7c0e63bb54ab025
MichelleDuda <michelle.ann.hannon@gmail.com>
2025-05-07 13:31:17 +0100

    Set up menu views and templates (issue #11)

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import MenuItem
 # Create your views here.
+
+
+def all_menu_items(request):
+    ''' A view to retrun the menu items'''
+
+    menu_items = MenuItem.objects.all()
+
+    context = {
+        'menu_items': menu_items,
+    }
+    
+    return render(request, 'menu/menu.html', context)
