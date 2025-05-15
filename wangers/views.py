ce4c273902b072a4f6cd3ebdbcf78a701aadfbfc
MichelleDuda <michelle.ann.hannon@gmail.com>
2025-05-15 09:23:32 +0100

    Add styling to error pages

--- wangers/views.py
+++ wangers/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+import logging
+logger = logging.getLogger(__name__)
 
 
 def handler404(request, exception):
+    logger.warning(f'404 Error: {exception}')
     return render(request, "errors/404.html", status=404)
 
 
