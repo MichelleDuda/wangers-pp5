b11bd239052c3a5d73e6c623589bd5c07c69bbde
MichelleDuda <michelle.ann.hannon@gmail.com>
2025-05-19 13:34:58 +0100

    Add initial set up for Stripe payments

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51RQSXBD5BVLL0zXK9nGwotI0MQw0ybu5fhuNcyBPHAEbNQgYdnIO6fS1AoMsWBq2cgs0nBgtFgnN14on2a596m9Q00lp4V5pSx',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
