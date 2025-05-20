6ed6d59ef0a26b4745345f2a0377a507a37ae18c
MichelleDuda <michelle.ann.hannon@gmail.com>
2025-05-20 08:30:39 +0100

    Add  and test form submission functionality to Stripe. Issue #8

--- checkout/views.py
+++ checkout/views.py
@@ -1,20 +1,39 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from .forms import OrderForm
+from django.conf import settings
+from cart.contexts import cart_contents
+import stripe
 
 # Create your views here.
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get ('cart', {})
     if not cart:
         messages.error(request, "There's nothing in your cart at the moment!")
         return redirect(reverse('menu'))
     
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+    
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51RQSXBD5BVLL0zXK9nGwotI0MQw0ybu5fhuNcyBPHAEbNQgYdnIO6fS1AoMsWBq2cgs0nBgtFgnN14on2a596m9Q00lp4V5pSx',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
