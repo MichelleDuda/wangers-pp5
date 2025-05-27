6b44359a4d8a72b806668ad18debd09dff9cf794
MichelleDuda <michelle.ann.hannon@gmail.com>
2025-05-27 12:46:08 +0100

    Added initial webhook setup. Issue #13

--- wangers/settings.py
+++ wangers/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', 'localhost',]
 
 
 # Application definition
@@ -206,6 +206,7 @@ STANDARD_DELIVERY_PERCENTAGE = 20
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
