0e5198b83719f30398a8585c8d3e8bc511a9f47e
MichelleDuda <michelle.ann.hannon@gmail.com>
2025-06-03 17:06:15 +0100

    Fix textarea overflow on contact form

--- home/forms.py
+++ home/forms.py
@@ -6,6 +6,14 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = ContactMessage
         fields = ['name', 'email', 'message']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'message': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 5,
+            }),
+        }
 
 
 class NewsletterSignupForm(forms.ModelForm):
