a4acbfa9110a8d200852a5c71970fdc775787d10
MichelleDuda <michelle.ann.hannon@gmail.com>
2025-06-03 07:49:04 +0100

    Create contact model, form, view. Issue #17

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import ContactMessage
 
-# Register your models here.
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'submitted_at')
+    list_filter = ('submitted_at',)
+    search_fields = ('name', 'email', 'subject', 'message')
+    ordering = ('-submitted_at',)
--- home/views.py
+++ home/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib import messages
+from .forms import ContactForm
 
-# Create your views here.
 
 def index(request):
-    ''' A view to retrun the home page'''
+    ''' A view to return the home page'''
     
     return render(request, 'home/index.html')
+
+
+def contact_view(request):
+    ''' A view to handle the contact page '''
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact = form.save()
+
+            send_mail(
+                "Thanks for contacting us!",
+                "Hi {},\n\nThanks for reaching out. We’ll get back to you shortly.\n\n- Your Restaurant Team".format(contact.name),
+                settings.DEFAULT_FROM_EMAIL,
+                [contact.email],
+                fail_silently=False,
+            )
+
+            messages.success(request, "Your message has been submitted. We'll be in touch soon!")
+            return redirect('contact')
+    else:
+        form = ContactForm()
+    return render(request, 'home/contact.html', {'form': form})
