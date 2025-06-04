07b5c33c699c701f95675e4e79dff43e2db64966
MichelleDuda <michelle.ann.hannon@gmail.com>
2025-06-04 16:12:22 +0100

    Fix CSS styling

--- wangers/settings.py
+++ wangers/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', 'localhost',]
 
@@ -226,5 +226,3 @@ else:
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
